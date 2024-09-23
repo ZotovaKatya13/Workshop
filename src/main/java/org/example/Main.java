1d3ee6a9aa50a06af6cc344577fb022d02e83f05
ZotovaKatya13 <katyaz19981998@mail.ru>
2024-09-23 16:39:56 +0400

    Update Main.java
    Add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static By buttonAddCustomer = (By.cssSelector(".btn.btn-lg.tab"));
     public static By postCodee = (By.cssSelector("div.form-group:nth-of-type(3)>input.ng-invalid-required"));
     public static By firstName = (By.cssSelector(".ng-untouched.ng-invalid.ng-invalid-required"));
+    public static By customers = (By.cssSelector(".btn.btn-lg.tab:nth-of-type(3)"));
 
     public static String generateRandomPostCode() {
         long randomNum = ThreadLocalRandom.current().nextLong(1000000000L, 10000000000L);
